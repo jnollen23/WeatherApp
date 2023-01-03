e548478c11bf5e79649336f734c5a993aab3bd3a
Jacob Nollen <jacob.nollen@hotmail.com>
2023-01-03 00:16:29 -0500

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let src = "https://api.openweathermap.org/data/2.5/forecast?";
+let apiKey = "f6b6c1815274dfb64ce0b52e2e0f5307";
+let previousCities = document.getElementById("previous-search");
+
+function fetchCords(name) {
+    fetch(`${src}q=${name}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.hasOwnProperty('city')) {
+                addCityToPrevious(name);
+                getWeather(data.city.coord.lat, data.city.coord.lon);
+            }
+            else console.log("City not found");
+        });
+}
+
+function getWeather(lat, lon) {
+    fetch(`${src}lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function addCityToPrevious(name) {
+    let newCity = document.createElement('div');
+    newCity.innerText = name;
+    previousCities.appendChild(newCity);
+}
+
+fetchCords("Lawrenceville")
