69d2b42e44494e8f2fe45692f68cda17212d1a23
Jacob Nollen <jacob.nollen@hotmail.com>
2023-01-09 00:02:54 -0500

    finished previous search storage and reacll. finished previous search removing previous search and put it on top after searching it again

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function fetchCords(name) {
         .then(response => response.json())
         .then(data => {
             if (data.hasOwnProperty('city')) {
-                addCityToPrevious(name);
+                citySearch(data.city.name);
                 getWeather(data.city.coord.lat, data.city.coord.lon);
             }
             else console.log("City not found");
@@ -30,8 +30,36 @@ function addCityToPrevious(name) {
     newCity.innerText = name;
     previousCities.appendChild(newCity);
 }
+function removeCities(){
+    previousCities.innerHTML = ""
+}
 
 function submitForm(event){
     event.preventDefault();
     fetchCords(document.search.searchInput.value);
 }
+
+function setupPreviousCities(){
+    const keys = localStorage.getItem("previousCities").split(',');
+    if(keys !== null)
+        keys.forEach(x=> addCityToPrevious(x));
+}
+
+function citySearch(cityName){
+    let prevCities = localStorage.getItem("previousCities");
+    if( prevCities === null){
+        localStorage.setItem('previousCities', cityName);
+    }
+    else{
+        prevCities = prevCities.split(',');
+        if(prevCities.includes(cityName)){
+            prevCities.splice(prevCities.indexOf(cityName), 1);
+        }
+        prevCities.unshift(cityName);
+        localStorage.setItem("previousCities", prevCities);
+    }
+    removeCities();
+    setupPreviousCities();
+}
+
+setupPreviousCities();
